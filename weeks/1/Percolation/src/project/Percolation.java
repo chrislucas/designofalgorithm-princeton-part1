de5783b00649919a33db55ca5589824f5185b8ad
Christoffer Lucas <christoffer.luccas@gmail.com>
2017-11-09 05:54:03 -0200

    estudo

--- weeks/1/Percolation/src/project/Percolation.java
+++ weeks/1/Percolation/src/project/Percolation.java
@@ -1,4 +1,19 @@
 package project;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+
+    private WeightedQuickUnionUF wqu;
+    private int qSites;
+
+    private static final int path [][] = {};
+
+    public Percolation(int s) {
+        wqu = new WeightedQuickUnionUF(s);
+    }
+
+    public static void main(String[] args) {
+    }
+
 }
--- weeks/1/Percolation/src/project/PercolationStats.java
+++ weeks/1/Percolation/src/project/PercolationStats.java
@@ -1,4 +1,11 @@
 package project;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class PercolationStats {
+
+    public static void main(String[] args) {
+
+    }
+
 }
