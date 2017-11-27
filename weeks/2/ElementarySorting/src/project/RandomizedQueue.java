da2282f443c779c49219088d566ad1b8f010804c
Christoffer Lucas <christoffer.luccas@gmail.com>
2017-11-27 01:22:05 -0200

    mais um arquivo

--- weeks/2/ElementarySorting/src/project/RandomizedQueue.java
+++ weeks/2/ElementarySorting/src/project/RandomizedQueue.java
@@ -1,8 +1,38 @@
 package project;
 
-public class RandomizedQueue {
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    private int controlSize;
+
+    public RandomizedQueue() {
+
+    }
+
+    public int size() {
+        return controlSize;
+    }
+
+    public void enqueue(Item item) {
+
+    }
+
+    public Item dequeue() {
+        return null;
+    }
+
+    public Item sample() {
+        return null;
+    }
+
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
 
     public static void main(String[] args) {
-        
+
     }
 }
