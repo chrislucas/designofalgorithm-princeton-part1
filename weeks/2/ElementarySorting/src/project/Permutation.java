d32aaad759164a7cfb7ccaa1a0c89c85a59e1f24
chrislucas <christoffer.luccas@gmail.com>
2017-12-07 17:17:51 -0200

    estudos

--- weeks/2/ElementarySorting/src/project/Permutation.java
+++ weeks/2/ElementarySorting/src/project/Permutation.java
@@ -1,13 +1,20 @@
 package project;
 
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
     public static void main(String[] args) {
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
         if(args != null && args.length > 0) {
-            int in = Integer.parseInt(args[0]);
+            int k = Integer.parseInt(args[0]);
             while (!StdIn.isEmpty()) {
-                String item = StdIn.readString();
+                String in = StdIn.readString();
+                    rq.enqueue(in);
+            }
+            if(!rq.isEmpty()) {
+                for(int i=0; i<k; i++)
+                    StdOut.println(rq.dequeue());
             }
         }
     }
