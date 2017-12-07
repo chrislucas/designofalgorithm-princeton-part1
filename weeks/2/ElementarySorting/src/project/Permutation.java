833fe85dfb99856bec2e27e0b055dd536dee49f7
chrislucas <christoffer.luccas@gmail.com>
2017-12-07 16:45:21 -0200

    implementando classe com uma fila randomica

--- weeks/2/ElementarySorting/src/project/Permutation.java
+++ weeks/2/ElementarySorting/src/project/Permutation.java
@@ -1,7 +1,14 @@
 package project;
 
+import edu.princeton.cs.algs4.StdIn;
+
 public class Permutation {
     public static void main(String[] args) {
-
+        if(args != null && args.length > 0) {
+            int in = Integer.parseInt(args[0]);
+            while (!StdIn.isEmpty()) {
+                String item = StdIn.readString();
+            }
+        }
     }
 }
--- weeks/2/ElementarySorting/src/project/RandomizedQueue.java
+++ weeks/2/ElementarySorting/src/project/RandomizedQueue.java
@@ -6,19 +6,17 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-
-    private int controlSize, head, tail;
-
     Item [] data;
-
+    private int controlSize, head, tail;
     public RandomizedQueue() {
-        data = (Item[]) new Object[2];
+        data = (Item[]) new Object[1];
         tail = head = controlSize = 0;
     }
 
     public boolean isEmpty() {
         return controlSize == 0;
     }
+
     public int size() {
         return controlSize;
     }
@@ -29,35 +27,85 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         if(controlSize == data.length)
             resize(data.length * 2);
         data[tail++] = item;
+        /**
+         * caso especial: Apos algumas delecoes a variavel que cuida do fim da
+         * lista pode aumentar mais rapido do que a que controla a quantidade de elementos
+         * nela e assim ultrapassar o tamanho maximo atual da estrutura de dados. Como
+         * nessa estrutura os elementos sao renovidos a partir da variavel que cuida do comeco
+         * da fila ('head') e sempre que necessario redimensiona-mos o array, os primeiros indices
+         * ficam vagos, assim podemos zerar a variavel que cuida do fim da lista, usa-la
+         * quando quisermos adicionar itens na lista, que quando chegar o momento, o algoritmo
+         * ira ajustar o tamanho da estrutura de dados, redefinir o valor da variavel que cuisa
+         * do final da fila e posicionar os elementos que estavam no começo da fila nesse caso
+         * especial para o final dela.
+         * */
         if(tail == data.length)
             tail = 0;
         controlSize++;
     }
 
-    private void resize(int size) {
-        Item [] items = (Item[]) new Object[size];
-        for(int idx=0; idx<data.length; idx++)
-            items[idx] = data[(head+idx)%data.length];
+    private void resize(int capacity) {
+        Item [] items = (Item[]) new Object[capacity];
+        int size = data.length;
+        /**
+         * Loop que vai de 0 ate o numero de itens atualmente
+         * na estrutura para refazer a estrutura com o novo tamano
+         * */
+        for(int idx=0; idx<controlSize; idx++)
+            /**
+             * Caso interessante: Adicione um numero de elementos
+             * do tipo 2^m, remova alguns elementos e depois adicione
+             * uma quantidade 2^m, onde m > n. I que ocorrera eh que
+             * a veriavel que cuida do começo da fila ('head') mais a quantidade
+             * de elementos que tem na fila ('controle') ira passar do tamanho
+             * limite da estrutura de dados atual ('data'), por isso usamos o operador
+             * modulo ('%') pois os elementos que estao numa posicao cujo valor 'head' +
+             * 'quantidade de itens' > size estao exatamente no comeco da lista e precisam
+             * ser reajustados para o final dela. Como o loop varia 0 <= idx < controlSize
+             * a instrucao (head+idx)%size garante que os elementos serao postos nos devidos
+             * lugares.
+             * */
+            items[idx] = data[(head+idx)%size];
         data = items;
         head = 0;
         tail = controlSize;
     }
 
+    /**
+     *         Item item = data[head];
+     *         data[head++] = null;
+     *         if(head == data.length)
+     *         head = 0;
+     * */
     public Item dequeue() {
         if(isEmpty())
             throw new NoSuchElementException();
-        Item item = data[head];
-        data[head++] = null;
+        int indexRandom = controlSize > 1 ? getRandomIndex() : 0;
         controlSize--;
-        if(head == data.length)
-            head = 0;
+        if(indexRandom == head) {
+            head++;
+            if(head == data.length)
+                head = 0;
+        }
+        Item item = data[indexRandom];
+        data[indexRandom] = null;
+        for(int idx=indexRandom; idx<controlSize; idx++) {
+            data[idx] = data[idx+1];
+            data[idx+1] = null;
+        }
+        tail = controlSize;
         if(controlSize > 0 && controlSize == data.length / 4)
             resize(data.length/2);
         return item;
     }
 
     private int getRandomIndex() {
-       return StdRandom.uniform(0, data.length-1);
+       //return StdRandom.uniform(0, controlSize);
+       return StdRandom.uniform(head == 0 ? 1 : head, tail == 0 ? 2 : tail);
+    }
+
+    private int getRandomIndex(int p, int q) {
+        return StdRandom.uniform(p, q);
     }
 
     public Item sample() {
@@ -66,19 +114,35 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         return data[getRandomIndex()];
     }
 
+    public int [] shuffle() {
+        int [] indexes = new int[controlSize];
+        for(int i=0; i<controlSize; i++) {
+            indexes[i] = i;
+        }
+        for(int i=0; i<controlSize; i++) {
+            int r = StdRandom.uniform(i+1);
+            int aux = indexes[r];
+            indexes[r] = indexes[i];
+            indexes[i] = aux;
+        }
+        return indexes;
+    }
+
     @Override
     public Iterator<Item> iterator() {
         return new Iterator<Item>() {
-            private int index = 0;
-            private int size = data.length;
+            private int indexes [] = shuffle();
+            private int  size = data.length, counter = 0;
             @Override
             public boolean hasNext() {
-                return index < controlSize;
+                return counter < controlSize;
             }
 
             @Override
             public Item next() {
-                return data[((index++)+head)%size];
+                if(!hasNext())
+                    throw new NoSuchElementException();
+                return data[((indexes[counter++])+head)%size];
             }
 
             @Override
@@ -89,21 +153,46 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     public static void test() {
-        RandomizedQueue<String> queue = new RandomizedQueue<>();
-        queue.enqueue("Maria");
-        queue.enqueue("Marta");
-        queue.enqueue("Joana");
-        queue.enqueue("Estela");
-        queue.enqueue("Gertrudes");
-        queue.enqueue("Penelope");
-        Iterator<String> iterator = queue.iterator();
+        RandomizedQueue<Integer> queue = new RandomizedQueue<>();
+        for(int i=0; i<(1<<4)+1; i++)
+            queue.enqueue(i);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        for (int i=7; i<(1<<6)+1; i++)
+            queue.enqueue(32*i);
+        Iterator<Integer> iterator = queue.iterator();
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
     }
 
+    public static void test1() {
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+        queue.enqueue("Maria");
+        queue.enqueue("Marta");
+        queue.enqueue("Joana");
+        queue.enqueue("Estela");
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue("Juliana");
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue("Adriana");
+        queue.enqueue("Amanda");
+        queue.enqueue("Milena");
+        queue.dequeue();
+        while (!queue.isEmpty())
+            System.out.println(queue.dequeue());
+        queue.enqueue("Milena");
+        queue.enqueue("Amanda");
+        while (!queue.isEmpty())
+            System.out.println(queue.dequeue());
+    }
 
     public static void main(String[] args) {
-        test();
+        //test();
+        //test1();
     }
 }
