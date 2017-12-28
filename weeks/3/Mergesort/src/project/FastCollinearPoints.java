11f43d0f3cf438eebd679334d3e5c35281622c55
chrislucas <christoffer.luccas@gmail.com>
2017-12-28 13:49:38 -0200

    trabalhando na solucao para o problema da terceira semana

--- weeks/3/Mergesort/src/project/FastCollinearPoints.java
+++ weeks/3/Mergesort/src/project/FastCollinearPoints.java
@@ -1,4 +1,49 @@
 package project;
 
-public class FastCollinearPoints {
+import javax.sound.sampled.Line;
+import java.util.Arrays;
+
+public class FastCollinearPoints implements Solver {
+    private Point[] points;
+
+    public FastCollinearPoints(Point[] points) {
+        this.points = points;
+        if(!verify(points))
+            throw new IllegalArgumentException();
+    }
+
+    private boolean verify(Point [] points) {
+        if(points == null)
+            return false;
+        for(Point p : points) {
+            if (p == null)
+                return false;
+        }
+        for (int i=0; i<points.length-1; i++) {
+            for (int j=i+1; j<points.length ; j++) {
+                if(points[i].compareTo(points[j]) == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public int numberOfSegments() {
+        int acc = 0;
+        for(LineSegment segment : segments()) {
+            if(segment != null)
+                acc++;
+        }
+        return acc;
+    }
+
+    @Override
+    public LineSegment [] segments() {
+        Arrays.sort(points, points[0].slopeOrder());
+        LineSegment [] segs = new LineSegment[points.length];
+        for (int i=0; i<points.length ; i++) {
+
+        }
+        return segs;
+    }
 }
---
+++ weeks/3/Mergesort/src/project/Main.java
@@ -0,0 +1,98 @@
+package project;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Classe para ler os arquivos de teste do projeto
+ * */
+
+public class Main {
+
+    private static final String [] paths = {
+         "C:\\Users\\r028367\\Downloads\\collinear-testing\\collinear"
+        ,"C:\\Users\\r028367\\Downloads\\collinear-testing\\collinear\\special"
+    };
+
+    private static void drawing(Solver solver, Point [] points) {
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points)
+            p.draw();
+        StdDraw.show();
+        // print and draw the line segments
+        for (LineSegment lineSegment : solver.segments()) {
+            StdOut.println(lineSegment);
+            lineSegment.draw();
+        }
+        StdDraw.show();
+    }
+
+    public static List<Point[]> readFiles() {
+        List<Point[]> listOfPoints = new ArrayList<>();
+        try {
+            File dir = new File(paths[1]);
+            File [] fs = dir.listFiles();
+            if(fs != null) {
+                for(File file : fs) {
+                    if(file.isFile()) {
+                        String name = file.getName();
+                        if( name.matches("^.*\\.txt") ) {
+                            String absolutePath = file.getAbsolutePath();
+                            System.out.printf("Arquivo %s\n", absolutePath);
+                            BufferedReader reader = new BufferedReader(new FileReader(absolutePath));
+                            /**
+                             * Aparentemente todos os arquivos de teste desse problema estao com
+                             * um formato parecido com de competicao de programacao. Comeca com um
+                             * inteiro N na primeira linha e depois segue N linhas com pares de inteiros
+                             * representando pontos num plano cartesiano
+                             * */
+                            int cases = Integer.parseInt(reader.readLine().trim());
+                            System.out.printf("%d pontos\n", cases);
+                            Point [] points = new Point[cases];
+                            int m = 0;
+                            String in = "";
+                            while ((in = reader.readLine()) != null) {
+                                // verifiquei um arquivo com linhas em branco
+                                if(in.equals(""))
+                                    continue;
+                                StringTokenizer tk = new StringTokenizer(in.trim(), " ");
+                                int x = Integer.parseInt(tk.nextToken());
+                                int y = Integer.parseInt(tk.nextToken());
+                                points[m++] = new Point(x, y);
+                            }
+                            listOfPoints.add(points);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return listOfPoints;
+    }
+
+    public static void bruteCollinearPointsTest(Point [] points) {
+        BruteCollinearPoints bruteCollinearPoints = new BruteCollinearPoints(points);
+        System.out.printf("Número de segmentos %d\n\n", bruteCollinearPoints.numberOfSegments());
+        drawing(bruteCollinearPoints, points);
+    }
+
+    public static void fastCollinearPointsTest(Point [] points) {
+        FastCollinearPoints fastCollinearPoints = new FastCollinearPoints(points);
+        System.out.printf("Número de segmentos %d\n\n", fastCollinearPoints.numberOfSegments());
+        drawing(fastCollinearPoints, points);
+    }
+
+    public static void main(String[] args) {
+        List<Point[]> listOfPoints = readFiles();
+        bruteCollinearPointsTest(listOfPoints.get(0));
+    }
+}
